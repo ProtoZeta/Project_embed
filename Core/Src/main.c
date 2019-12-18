a19c2dadae49c550aaf6d5ddc7395f1009115e78
unknown <infinitereload@gmail.com>
2019-12-18 11:14:39 +0700

    intialize PWM at tim3 ch1,3 GPIO B4B5C8C9

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -415,6 +415,10 @@ static void MX_TIM3_Init(void)
   sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
